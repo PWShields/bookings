cb873e1f994d7d121acc364d433d7add3f8bcecb
pws-nla <pshields@nla.gov.au>
2018-09-26 14:58:38 +1000

    Modify enums

--- src/main/java/com/puffinpowered/bookings/domain/TicketType.java
+++ src/main/java/com/puffinpowered/bookings/domain/TicketType.java
@@ -2,8 +2,21 @@ package com.puffinpowered.bookings.domain;
 
 public enum TicketType {
 
-    STANDARD,
-    PREMIUM,
-    VIP,
-    BACKSTAGE
+    STANDARD("This is a standard ticket"),
+    PREMIUM("This is a premium ticket"),
+    VIP("This is VIP"),
+    BACKSTAGE("This ticket allows you backstage before the show");
+
+    private String description;
+
+    TicketType(String description) {
+        this.description = description;
+    }
+
+    private TicketType() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
